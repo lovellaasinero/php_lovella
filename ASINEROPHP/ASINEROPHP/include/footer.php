8d84297ffdd2a67eedb80e2411b7713f03ce8900
lovellaasinero <128472285+lovellaasinero@users.noreply.github.com>
2023-04-04 19:37:24 +0800

    Add files via upload

--- ASINEROPHP/ASINEROPHP/include/footer.php
+++ ASINEROPHP/ASINEROPHP/include/footer.php
@@ -7,6 +7,18 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+    <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+    $( "#anim" ).on( "change", function() {
+      $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+    });
+  } );
+  </script>
+
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
--- ASINEROPHP/ASINEROPHP/index.php
+++ ASINEROPHP/ASINEROPHP/index.php
@@ -13,9 +13,24 @@
     <input type="last name" class="form-control" id="exampleInputLastname1">
   </div>
   <div class="form-group">
-    <label for="birthdaytime">Birth date</label><br>
-    <input type="datetime-local" id="birthdaytime" name="birthdaytime">
+    <label for="birthday">Birth date</label><br>
+    <input type="text" class="form-control" id="datepicker" name="birthday"> 
+    <p>Animations:<br>
+  <select id="anim">
+    <option value="show">Show (default)</option>
+    <option value="slideDown">Slide down</option>
+    <option value="fadeIn">Fade in</option>
+    <option value="blind">Blind (UI Effect)</option>
+    <option value="bounce">Bounce (UI Effect)</option>
+    <option value="clip">Clip (UI Effect)</option>
+    <option value="drop">Drop (UI Effect)</option>
+    <option value="fold">Fold (UI Effect)</option>
+    <option value="slide">Slide (UI Effect)</option>
+    <option value>None</option>
+  </select>
+</p>
   </div>
+  
   <div class="form-group">
     <label for="exampleInputSpecialty1">Specialty</label><br>
         <select class="form-select" aria-label="Default select example">
