a5086c7bad3cf2ead951311f59f65bcaad5f0031
lovellaasinero <128472285+lovellaasinero@users.noreply.github.com>
2023-04-04 19:24:34 +0800

    Add files via upload

--- ASINEROPHP/ASINEROPHP/include/header.php
+++ ASINEROPHP/ASINEROPHP/include/header.php
@@ -7,7 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
     <title>LOVELLA</title>
 
   </head>
